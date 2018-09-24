ef30e5392aaa03c2da3279dbf6eca3b62cb191f8
cilous <ben.004@hotmail.com>
2018-09-24 19:03:21 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -23,81 +23,55 @@ function setContent(content) {
 
 function parseMarkDown(content) {
     // split content for each paragraph
-    const content_splited = R.split(/\r\n\r\n/, content)
-    
-    // const addTag = x => {
-    //     if ( R.test(/^#/, x) ) {
-    //         const count = R.match(/^#+/g, x)[0]
-    //         const temp = R.replace(count, '', x)
-    //         const h = '<h' + count.length + '>' + temp + '</h' + count.length + '>'
-    //         return h
-    //     } else {
-    //         if (R.test(/\*(.*?)\*/g, x)) {
-    //             const bold = R.match(/\*(.*?)\*/g, x)
-    //             const addBold = text => {
-    //                 const textWithoutStar = R.replace(/\*/g,'',text)
-    //                 x = R.replace(text,'<b>' + textWithoutStar + '</b>' ,x)
-    //             }
-    //             R.forEach(addBold, bold)
-    //         }
-    //         const p = '<p>' + x + '</p>'
-    //         return p
-    //     }
-    // }
-    
-    
-    // const match = (condition, input) => R.match(condition,input)
-    // const test = (condition, input) => R.test(condition,input)
-    const blank = ''
-    const replaceAddReplaceInput = (textReplace,input) => (textFind) => R.replace(textFind,textReplace,input)
+    const content_splited = R.split(/\n\n/, content)
 
+    const BLANK = ''
+    const replace = (textReplace,input) => (textFind) => R.replace(textFind,textReplace,input)
+    
     const addTagH = input => {
-        const tagHReplaceAddTextFind = replaceAddReplaceInput(blank, input)
+        const replace_tagH_addTextFind = replace(BLANK, input)
         // Search if input start with '#' pattern then add tag <h#> 
         if ( R.test(/^#/, input) ) {
             const count = R.match(/^#+/g, input)[0]
-            const temp = tagHReplaceAddTextFind(count)
-            const tagH = '<h' + count.length + '>' + temp + '</h' + count.length + '>'
+            const text = replace_tagH_addTextFind(count)
+            const tagH = '<h' + count.length + '>' + text + '</h' + count.length + '>'
             return tagH
         } else {
-            // if not start with '#' will return input with no touch
+            // if not start with '#' will return (input) with no touch
             return input
         }
     }
 
     const addTagP  = input => {
         // Search if input not start with <h#> from previous function will add tag <p> 
-        if ( !R.test(/^<h/g ,input) ) {
-            const tagP = '<p>' + input + '</p>'
+        if ( !R.test(/^<h/g, input) ) {
+            const tagP = '<p>' + input + '<p/>'
             return tagP
         } else {
             return input
         }
     }
 
+
     const addTagB = input => {
         // Search if input are include '*' then convert to tag <b> 
         if ( R.test(/\*(.*?)\*/g, input) ) {
-            const findBold = R.match(/\*(.*?)\*/g, input)
+            let inputEdited = input
+            const textWithStar = R.match(/\*(.*?)\*/g, input)
             const addBold = text => {
-                const tagBReplaceAddTextFind = replaceAddReplaceInput(blank, text)
-                const textWithoutStar = tagBReplaceAddTextFind(/\*/g)
-                input = R.replace(text,'<b>' + textWithoutStar + '</b>' ,input)
-                return input
+                const replace_tagB_addTextFind = replace(BLANK, text)
+                const textWithoutStar = replace_tagB_addTextFind(/\*/g)
+                inputEdited = R.replace(text,'<b>' + textWithoutStar + '</b>' ,inputEdited)
+                return inputEdited
             }
-            const tagB = R.map(addBold, findBold)
-            console.log(tagB)
+            const tagB = R.map(addBold, textWithStar)
+            console.log(tagB);
             return R.takeLast(1,tagB)
-            
         } else {
             return input
         }
     }
-    // consequence? may be bad?
-    const content_tagH = R.map(addTagH, content_splited)
-    const content_tagP = R.map(addTagP, content_tagH)
-    const content_tagB = R.map(addTagB, content_tagP).reduce(R.concat)
-
-    // const temp = R.map(addTag, content_splited).reduce(R.concat,'')
-    return content_tagB
+    const composeAddTag = R.compose(R.map(addTagB),R.map(addTagP),R.map(addTagH))
+    const content_html = R.reduce(R.concat,BLANK,composeAddTag(content_splited))
+    return content_html
 }
